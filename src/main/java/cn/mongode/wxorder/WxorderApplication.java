090541776229996a2f3a37790a39a7df482b07a2
MongoDe <mongode@163.com>
2018-04-20 11:44:38 +0800

    wxorder-demo
    初始化

---
+++ src/main/java/cn/mongode/wxorder/WxorderApplication.java
@@ -0,0 +1,12 @@
+package cn.mongode.wxorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxorderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxorderApplication.class, args);
+    }
+}
---
+++ src/test/java/cn/mongode/wxorder/WxorderApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.mongode.wxorder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WxorderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
